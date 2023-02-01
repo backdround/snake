f7c677e39ccc3a5dca90ee5938bc2e6d7cd9e979
Vlad Chepaykyn <backdround@yandex.ru>
2023-02-01 16:07:37 +0300

    move sources to src directory

--- run.lua
+++ run.lua
@@ -1,9 +1,13 @@
 #!/usr/bin/env lua
 
-local version = _VERSION:match("%d+%.%d+")
+local version = _VERSION:match('%d+%.%d+')
 
+-- Modules
 package.path = "./lua_modules/share/lua/" .. version .. "/?.lua;" .. package.path
 package.path = "./lua_modules/share/lua/" .. version .. "/?/init.lua;" .. package.path
 package.cpath = "./lua_modules/lib/lua/" .. version .. "/?.so;;" .. package.cpath
 
-dofile("./main.lua")
+-- Sources
+package.path = "./src/?.lua;" .. package.path
+
+dofile("./src/main.lua")
